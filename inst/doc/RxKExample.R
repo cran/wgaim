0a0b3a3d01b27fb3beab91632b21f29e7c297da3
Julian Taylor <julian.taylor@adelaide.edu.au>
2019-08-28 10:40:05 +0000

    version 2.0-0

--- inst/doc/RxKExample.R
+++ inst/doc/RxKExample.R
@@ -1,80 +1,80 @@
+## ---- include = FALSE----------------------------------------------------
+library(wgaim)
+library(ggplot2)
+knitr::opts_chunk$set(fig.path = "", highlight = FALSE)
 
-########### RAC875-Kukri Example
-########### Example 1:
+## ------------------------------------------------------------------------
+data(package = "wgaim")
 
-####### base model and diagnostics
-
+## ------------------------------------------------------------------------
 wgpath <- system.file("extdata", package = "wgaim")
+list.files(wgpath)
 
+## ------------------------------------------------------------------------
 data(phenoRxK, package = "wgaim")
+head(phenoRxK)
 
-rkyld.asi <- asreml(yld ~ Type, random = ~ Genotype + Rep,
-                   rcov = ~ ar1(Range):ar1(Row), data = phenoRxK)
+## ------------------------------------------------------------------------
+rkyld.asi <- asreml::asreml(yld ~ Type, random = ~ Genotype + Rep, residual =
+     ~ ar1(Range):ar1(Row), data = phenoRxK)
 
+## ------------------------------------------------------------------------
 summary(rkyld.asi)$varcomp
-plot(rkyld.asi)
-plot(variogram(rkyld.asi))
 
-row.ind <- c(1,seq(4, 20, by = 3))
-xyplot(resid(rkyld.asi) ~ Range | Row, data = phenoRxK, type = "b",
-     panel = function(x, y, ...){
-           panel.abline(h = 0, lty = 2)
-           panel.xyplot(x, y, ...)},
-     aspect = 4/5, layout = c(9,3), ylab = "Residuals",
-     scales = list(x = list(at = row.ind,labels = phenoRxK$Row[row.ind])))
+## ----variogram, fig.align = "center", out.width = "95%", dpi = 150-------
+plot(asreml::varioGram.asreml(rkyld.asi))
 
-rkyld.asf <- asreml(yld ~ Type + lrange, random = ~ Genotype + Rep + Range,
-                   rcov = ~ ar1(Range):ar1(Row), data = phenoRxK)
+## ----residuals, warning = FALSE, fig.align = "center", fig.width = 13, fig.height = 10, out.width = "95%", dpi = 150----
+phenoRxKd <- cbind.data.frame(phenoRxK, Residuals = resid(rkyld.asi))
+ggplot(phenoRxKd, aes(y = Residuals, x = as.numeric(Range))) + facet_wrap(~ Row) +
+    geom_hline(yintercept = 0, linetype = 2) + geom_point(shape = 16, colour = "blue") +
+    xlab("Range") + theme_bw()
 
-summary(rkyld.asf)$varcomp
-plot(rkyld.asf)
-plot(variogram(rkyld.asf))
+## ------------------------------------------------------------------------
+rkyld.asf <- asreml::asreml(yld ~ Type + lrow, random = ~ Genotype + Range, residual =
+     ~ ar1(Range):ar1(Row), data = phenoRxK)
 
-xyplot(resid(rkyld.asf) ~ Row | Range, data = phenoRxK, type = "b",
-     panel = function(x, y, ...){
-           panel.abline(h = 0, lty = 2)
-           panel.xyplot(x, y, ...)},
-     aspect = 4/5, layout = c(9,3), ylab = "Residuals",
-     scales = list(x = list(at = row.ind, labels = phenoRxK$Row[row.ind])))
-
-######## genetic data
-
+## ------------------------------------------------------------------------
 data(genoRxK, package = "wgaim")
-genoRxK <- read.cross("csvr", file="genoRxK.csv", genotypes=c("AA","BB"),
-        dir = wgpath, na.strings = c("-", "NA"))
 
-names(genoRxK)
-genoRxK$pheno[["Genotype"]][1:18]
+## ------------------------------------------------------------------------
+genoRxK <- read.cross(format = "csvr", file="genoRxK.csv", genotypes=c("AA","BB"),
+     dir = wgpath, na.strings = c("-", "NA"))
+
+## ------------------------------------------------------------------------
 summary(genoRxK)
+names(genoRxK$pheno)
 
-names(genoRxK$geno$"3D")
-genoRxK$geno$"3D"$data[200:208,1:8]
-genoRxK$geno$"3D"$map
+## ------------------------------------------------------------------------
+genoRxKi <- cross2int(genoRxK, consensus.mark = TRUE, impute = "MartinezCurnow",
+                      id = "Genotype")
 
-genoRxK <- cross2int(genoRxK, missgeno = "Mart", id = "Genotype", rem.mark = TRUE)
-class(genoRxK)
-names(genoRxK$geno$"3D")
-genoRxK$geno$"3D"$map
-genoRxK$geno$"3D"$imputed.data[200:208,1:8]
-genoRxK$geno$"3D"$intval[200:208,1:6]
+## ------------------------------------------------------------------------
+names(genoRxKi$geno[[1]])
 
-####### QTL analysis and breakout
+## ---- warning = FALSE, eval = FALSE--------------------------------------
+#  rkyld.qtl <- wgaim(rkyld.asf, genoRxKi, merge.by = "Genotype", fix.lines = TRUE,
+#                      gen.type = "interval", method = "random", selection = "interval",
+#                      trace = "rxk.txt", na.action = asreml::na.method(x = "include"))
 
-rkyld.qtl0 <- wgaim(rkyld.asf, phenoData = phenoRxK, intervalObj = genoRxK,
-merge.by = "Genotype", trace = TRUE, na.method.X = "include", breakout = 1,
-exclusion.window = 0)
+## ---- warning = FALSE, eval = TRUE---------------------------------------
+rkyld.asf <- asreml::update.asreml(rkyld.asf, fixed. = . ~ . - Type)
+rkyld.qtl <- wgaim(rkyld.asf, genoRxKi, merge.by = "Genotype", fix.lines = TRUE,
+                    gen.type = "interval", method = "random", selection = "interval",
+                    trace = "rxk.txt", na.action = asreml::na.method(x = "include"))
 
-out.stat(rkyld.qtl0, genoRxK, iter = 1, stat= "blups")
-out.stat(rkyld.qtl0, genoRxK, iter = 1, stat= "os")
+## ------------------------------------------------------------------------
+names(rkyld.qtl$QTL)
+names(rkyld.qtl$QTL$diag)
+class(rkyld.qtl)
 
-asreml:::summary.asreml(rkyld.qtl0)$varcomp
+## ------------------------------------------------------------------------
+summary(rkyld.qtl, genoRxKi)
 
-####### QTL analysis and summary
+## ----out, warning = FALSE, fig.align = "center", fig.width = 13, fig.height = 6, out.width = "95%", dpi = 150----
+outStat(rkyld.qtl, genoRxKi, iter = 1:2, statistic = "outlier")
+outStat(rkyld.qtl, genoRxKi, iter = 1:2, statistic = "blups")
 
-rkyld.qtl1 <- wgaim(rkyld.asf, phenoData = phenoRxK, intervalObj = genoRxK,
-merge.by = "Genotype", trace = TRUE, na.method.X = "include", exclusion.window = 0)
+## ----link, fig.align = "center", fig.width = 13, fig.height = 6, out.width = "95%", dpi = 150----
+linkMap(rkyld.qtl, genoRxKi)
 
-summary(rkyld.qtl1, genoRxK, LOD = FALSE)
-out.stat(rkyld.qtl1, genoRxK, iter = 1:5, stat= "blups")
-
-############ end script
--- inst/doc/SxTExample.R
+++ inst/doc/SxTExample.R
@@ -1,3 +1,4 @@
+## 2019 wgaim 2.0.x script
 
 ########### Sunco-Tasman Example
 ########### Example 2:
@@ -10,21 +11,34 @@ wgpath <- system.file("extdata", package = "wgaim")
 
 data("phenoSxT", package = "wgaim")
 phenoSxT <- asreml.read.table(paste(wgpath,"\\phenoSxT.csv", sep =""), header = TRUE, sep = ",")
-st.fmI <- asreml(myield ~ Type, random = ~ id + Rep + Range:Row + Millday, rcov = ~ Millday:ar1(Millord), data = phenoSxT)
-xyplot(resid(st.fmI) ~ as.numeric(Millord) | Millday, data = phenoSxT, type = "b")
+
+st.fmI <- asreml(myield ~ Type, random = ~ id + Rep + Range:Row + Millday, residual = ~ Millday:ar1(Millord), data = phenoSxT)
+
+phenoSxTd <- cbind.data.frame(phenoSxT, Residuals = resid(st.fmI))
+
+ggplot(phenoSxTd, aes(y = Residuals, x = as.numeric(Millord))) + facet_wrap(~ Millday) +
+    geom_hline(yintercept = 0, linetype = 2) + geom_point(shape = 16, colour = "blue") +
+    xlab("Mill order") + theme_bw()
+
 field.resid <- coef(st.fmI, pattern = "Range:Row")
 rrd <- data.frame(field.resid = field.resid, Range = factor(rep(1:12, each = 31)), Row = rep(1:31, 12))
-xyplot(field.resid ~ Row | Range, data = rrd, type = "b")
+
+ggplot(rrd, aes(y = field.resid, x = Row)) + facet_wrap(~ Range) +
+    geom_hline(yintercept = 0, linetype = 2) + geom_point(shape = 16, colour = "blue") +
+    geom_line(colour = "blue") + xlab("Row") + ylab("Field Residuals") + theme_bw()
+
 summary(st.fmI)$varcomp
 
 ####### full model
 
-st.fmF <- asreml(myield ~ Type + lord + lrow, random = ~ id + Rep + Range:Row + Millday, rcov = ~ Millday:ar1(Millord), data = phenoSxT, na.method.X = "include")
+st.fmF <- asreml(myield ~ Type + lord + lrow, random = ~ id + Rep + Range:Row + Millday,
+                 residual = ~ Millday:ar1(Millord), data = phenoSxT,
+                 na.action = na.method(x = "include"))
 summary(st.fmF)$varcomp
 
 # fit null model (for comparison)
 
-st.fmN <- asreml(myield ~ 1, random = ~ id, data = phenoSxT, na.method.X = "include")
+st.fmN <- asreml(myield ~ 1, random = ~ id, data = phenoSxT, na.action = na.method(x = "include"))
 
 ####### read in genetic data and check
 
@@ -35,32 +49,30 @@ nmar(genoSxT)
 nt <- ntyped(genoSxT, "ind")
 nt[nt < 120]
 genoSxT <- subset(genoSxT, ind = 1:180)
-genoSxT <- cross2int(genoSxT, missgeno="Mart", id = "id", rem.mark = TRUE)
+genoSxT <- cross2int(genoSxT, consensus.mark = TRUE, id = "id", impute = "MartinezCurnow")
 nmar(genoSxT)
 
 # plot the linkage map in various ways
 
-link.map(genoSxT, chr = names(nmar(genoSxT)), m.cex = 0.5)
-link.map(genoSxT, names(nmar(genoSxT)), m.cex = 0.5,
+linkMap(genoSxT, names(nmar(genoSxT)), m.cex = 0.5)
+linkMap(genoSxT, names(nmar(genoSxT)), m.cex = 0.5,
          chr.dist = list(start = 25, end = 180), marker.names = "dist")
 
 ####### wgaim QTL analyses for Full and Null model and diagnostics
 
-st.qtlN <- wgaim(st.fmN, phenoData = phenoSxT, intervalObj = genoSxT,
-    merge.by = "id", gen.type = "interval", method = "fixed",
-    selection = "interval", trace = "nullmodel.txt", exclusion.window = 0)
+st.qtlN <- wgaim(st.fmN, genoSxT, merge.by = "id", gen.type = "interval", method = "fixed",
+    selection = "interval", trace = "nullmodel.txt")
 
-st.qtlF <- wgaim(st.fmF, phenoData = phenoSxT, intervalObj = genoSxT,
-    merge.by = "id", gen.type = "interval", method = "fixed",
-    selection = "interval", trace = "fullmodel.txt", exclusion.window = 0)
+st.qtlF <- wgaim(st.fmF, genoSxT, merge.by = "id", gen.type = "interval", method = "fixed",
+    selection = "interval", trace = "nullmodel.txt")
 
 # diagnostics
 
 # plot the outlier statistics
 
-out.stat(st.qtlF, genoSxT, int = TRUE, iter = 1:2, cex = 0.6, stat = "os")
-out.stat(st.qtlF, genoSxT, int = TRUE, iter = 1:2, cex = 0.6, stat = "blups")
-out.stat(st.qtlF, genoSxT, int = TRUE, iter = 1:5, cex = 0.6, chr = c("2B","4B","6B","7D"))
+outStat(st.qtlF, genoSxT, iter = 1:2, statistic = "outlier")
+outStat(st.qtlF, genoSxT, iter = 1:2, statistic = "blups")
+outStat(st.qtlF, genoSxT, iter = 1:5, chr = c("2B","4B","6B","7D"))
 
 # trace of forward selection proces
 
@@ -70,72 +82,65 @@ tr(st.qtlF, iter = 1:10, digits = 3)
 
 # summary and table
 
+summary(st.qtlF, genoSxT, LOD = TRUE)
 summary(st.qtlN, genoSxT, LOD = TRUE)
 qtlTable(st.qtlF, st.qtlN, intervalObj = genoSxT, labels = c("Full", "Null"), columns = 1:8)
 
 # plot qtl on linkage map in various ways
 
-link.map(st.qtlF, genoSxT, marker.names = "dist", trait.labels = "Full")
-link.map(st.qtlF, genoSxT, chr = c("1B", "2B", "5D"), marker.names = "dist", trait.labels = "Full")
-link.map(st.qtlF, genoSxT, chr.dist = list(start = 25), marker.names = "dist", trait.labels = "Full")
+linkMap(st.qtlF, genoSxT, marker.names = "dist", trait.labels = "Full")
+linkMap(st.qtlF, genoSxT, chr = c("1B", "2B", "4D"), marker.names = "dist", trait.labels = "Full")
 
 # with both models
 
-link.map.default(list(st.qtlF, st.qtlN), genoSxT, marker.names = "dist",
+linkMap.default(list(st.qtlF, st.qtlN), genoSxT, marker.names = "dist",
                  trait.labels = c("Full", "Null"), list.cex = list(m.cex = 0.7))
-link.map.default(list(st.qtlF, st.qtlN), genoSxT, marker.names = "dist",
-                 trait.labels = c("Full", "Null"), chr.dist = list(start = 25))
-link.map.default(list(st.qtlF, st.qtlN), genoSxT, chr = c("1B", "2B", "5D"),
-                 marker.names = "dist", trait.labels = c("Full", "Null"),
-                 chr.dist = list(start = 50))
+linkMap.default(list(st.qtlF, st.qtlN), genoSxT, marker.names = "dist",
+                 trait.labels = c("Full", "Null"))
+linkMap.default(list(st.qtlF, st.qtlN), genoSxT, chr = c("1B", "2B", "4D"),
+                 marker.names = "dist", trait.labels = c("Full", "Null"))
 
 # customize your qtl plots
 
-link.map.default(list(st.qtlF, st.qtlN), genoSxT, marker.names = "dist",
+linkMap.default(list(st.qtlF, st.qtlN), genoSxT, marker.names = "dist",
     trait.labels = c("Full", "Null"), list.col = list(q.col = c("skyblue3",
-    "salmon2"), m.col = "red", t.col = c("skyblue3", "salmon2")), col = "gray")
+    "salmon2"), m.col = "red", t.col = c("skyblue3", "salmon2")))
 
-link.map.default(list(st.qtlF, st.qtlN), genoSxT, marker.names = "dist",
+linkMap.default(list(st.qtlF, st.qtlN), genoSxT, marker.names = "dist",
     trait.labels = c("Full", "Null"), list.col = list(q.col = rep(gray(0.8), 2),
     m.col = "black", t.col = "black"), list.cex = list(t.cex = 0.8), col = "gray")
 
 ####### whole genome marker analysis and summary
 
-st.qtlFM <- wgaim(st.fmF, phenoData = phenoSxT, intervalObj = genoSxT,
-                 merge.by = "id", gen.type = "marker", method = "fixed",
-                 selection = "interval", trace = "fullmodel.txt", exclusion.window = 0)
+st.qtlFM <- wgaim(st.fmF, genoSxT, merge.by = "id", gen.type = "marker", method = "fixed",
+                 selection = "interval", trace = "fullmodel.txt")
 
 summary(st.qtlFM, genoSxT, LOD = TRUE)
-out.stat(st.qtlFM, genoSxT, int = TRUE, iter = 1, cex = 0.6, stat = "blups")
-out.stat(st.qtlFM, genoSxT, int = TRUE, iter = 1:5, cex = 0.6)
-out.stat(st.qtlFM, genoSxT, int = TRUE, iter = 1:5, cex = 0.6, chr = c("2B","4B","5A", "6B","7D"))
-link.map(st.qtlFM, genoSxT, marker.names = "dist", trait.labels = "Full", cex = 2.0, pch = 16)
+
+outStat(st.qtlFM, genoSxT, iter = 1:5, statistic = "blups")
+outStat(st.qtlFM, genoSxT, iter = 1:5, chr = c("2B","4B","5A","6B","7D"))
+
+linkMap(st.qtlFM, genoSxT, chr.dist = list(start = 15), trait.labels = "Full", cex = 2.0, pch = 16)
+linkMap(st.qtlFM, genoSxT, trait.labels = "Full", cex = 2.0, pch = 16)
 
 # Null model
 
-st.qtlNM <- wgaim(st.fmN, phenoData = phenoSxT, intervalObj = genoSxT,
-                 merge.by = "id", gen.type = "marker", method = "fixed",
-                 selection = "interval", trace = "nullmodel.txt", exclusion.window = 0)
+st.qtlNM <- wgaim(st.fmN, genoSxT, merge.by = "id", gen.type = "marker", method = "fixed",
+                 selection = "interval", trace = "nullmodel.txt")
 
 # linkage map with QTL
 
-link.map.default(list(st.qtlFM, st.qtlNM), genoSxT, marker.names = "dist",
+linkMap.default(list(st.qtlFM, st.qtlNM), genoSxT, marker.names = "dist",
     trait.labels = c("Full", "Null"), list.col = list(q.col = c("red",
     "light blue"), m.col = "red", t.col = c("red", "light blue")),
     list.cex = list(t.cex = 0.9, m.cex = 0.7), col = "black", cex = 2, pch = 16)
 
-link.map.default(list(st.qtlFM, st.qtlNM), genoSxT, marker.names = "dist",
-    chr.dist = list(start = 50), trait.labels = c("Full", "Null"), list.col = list(q.col = c("red",
-    "light blue"), m.col = "red", t.col = c("red", "light blue")),
-    list.cex = list(t.cex = 0.9, m.cex = 0.7), col = "black", cex = 2, pch = 16)
+####### Random formulation
 
-####### Random formulation and exclusion window
+outStat(st.qtlF, genoSxT, iter = c(1,2,5), chr = c("1B","2B","6B"), statistic = "blups")
 
-out.stat(st.qtlF, genoSxT, int = TRUE, iter = c(2,3,11), cex = 0.6, chr = c("6B","7D"), stat = "blups")
-
-st.qtlFR <- wgaim(st.fmF, phenoData = phenoSxT, intervalObj = genoSxT,
-                 merge.by = "id", gen.type = "interval", method = "random",
-                 selection = "interval", trace = "fullmodel.txt", exclusion.window = 20)
+st.qtlFR <- wgaim(st.fmF, genoSxT, merge.by = "id", gen.type = "interval", method = "random",
+                 selection = "interval", trace = "fullmodel.txt")
 
 summary(st.qtlFR, genoSxT)
 
